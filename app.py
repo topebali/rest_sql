ce280922b1ab163a54a8db06e733ad790c138575
Michael Bincom <michael.bincom@gmail.com>
2019-11-28 09:56:23 +0100

    new commit

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask_migrate import Migrate
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 
 app = Flask(__name__)
 
@@ -26,6 +27,11 @@ migrate = Migrate(app, db)
 
 api = Api(app)
 
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 #from db import db
 #db.init_app(app)
 
@@ -33,9 +39,10 @@ jwt = JWT(app, authenticate, identity) #/auth
 
 
 
-
+api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 
--- resources/item.py
+++ resources/item.py
@@ -1,14 +1,20 @@
-import sqlite3
+
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
+
 
 class Item(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('price',
         type=float,
         required=True,
-        help='Ths field cannot be left blank!'
+        help='This field cannot be left blank!'
+    )
+    parser.add_argument('store_id',
+        type=int,
+        required=True,
+        help='Every item needs a store id!'
     )
 
     @jwt_required()
@@ -27,7 +33,7 @@ class Item(Resource):
 
         data = Item.parser.parse_args()
         
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, data['price'], data['store_id'])
 
         try:
             item.save_to_db()
@@ -36,11 +42,9 @@ class Item(Resource):
 
         return item.json(), 201  #it shows the objected has been created
 
-    
-
 
     def delete(self, name):
-        item = Item.find_by_name(name)
+        item = ItemModel.find_by_name(name)
         if item:
             item.delete_from_db()
 
@@ -53,10 +57,11 @@ class Item(Resource):
         #item = ItemModel(name, data['price'])
 
         if item is None:
-            item =  ItemModel(name, data['price'])
+            item =  ItemModel(name, data['price'], data['store_id'])
 
         else:
-            item.price =  data['price']
+            item.price = data['price']
+            item.store = data['store_id']
 
         item.save_to_db()
         
@@ -65,18 +70,6 @@ class Item(Resource):
 
 
 class ItemList(Resource):
+    
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items"
-        result = cursor.execute(query)
-
-        items = []
-        for row in result:
-            items.append({'name': row[0], 'price': row[1]})
-
-        
-        connection.close()
-
-        return {'items': items}
+        return {'items': [x.json() for x in ItemModel.query.all()]}
